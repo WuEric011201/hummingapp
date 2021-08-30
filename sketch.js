f9fc4e70aeb79206873694ab15bc32340a8af759
WuEric011201 <77968284+WuEric011201@users.noreply.github.com>
2021-08-30 10:48:32 +0800

    Cleared up code

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-// var notes = [yi, er, san, si, wu, liu, qi, ba, jiu, shi, shiyi, shier, shisan, shisi, shiwu, shiliu];
+
 var counter = 0;
 var inrterval;
 var button;
@@ -14,23 +14,7 @@ function setup() {
   button = createButton('start');
   button.mousePressed(draw);
   
-
 }
-
-// function doTimer(){
-//   interval = setInterval(timeIt, 500000000000000000000);
-//   button.html('stop timer');
-// }
-
-// function timeIt(){
-//   timer.html(counter);
-//   counter++;
-
-// }
-
-
-
-
 
 function draw() {
  
@@ -53,76 +37,56 @@ function draw() {
 
   fre = fft.getCentroid();
 
-  //   var nyquist = 22050;
-  //   // display
-  //   var mean_freq_index = fre / (nyquist / spectrum.length);
-
-  //   //console.log(log(1024));
-  //   centroidplot = map(log(mean_freq_index), 0, log(spectrum.length), 100, 0);
-
-
-  
-  // var x =0
-  // stroke(51);
-  // rect(x, 0, 5, height);
-  // x = x+3;
-  // if (x > width){
-  //   x = 0;
-  // }
-
   stroke(0, 0, 0); // black note
- fre = fft.getCentroid();
-  var wuxiany
+  fre = fft.getCentroid();
+  var yaxis
  for ( i =0; i < width; i+= 20) {
     if (fre > 311 && fre < 330) {
-      wuxiany = 60;
-      ellipse(i, wuxiany, 10, 7)
+      yaxis = 60;
+      ellipse(i, yaxis, 10, 7)
     } else if (fre > 330 && fre < 369) {
-      wuxiany = 55;
-      ellipse(i, wuxiany, 10, 7)
+      yaxis = 55;
+      ellipse(i, yaxis, 10, 7)
     } else if (fre > 369 && fre < 415) {
-      wuxiany = 50;
-      ellipse(i, wuxiany, 10, 7)
+      yaxis = 50;
+      ellipse(i, yaxis, 10, 7)
     } else if (fre > 415 && fre < 466) {
-      wuxiany = 45;
-      ellipse(i, wuxiany, 10, 7)
+      yaxis = 45;
+      ellipse(i, yaxis, 10, 7)
     } else if (fre > 466 && fre < 508) {
-      wuxiany = 40;
-      ellipse(i, wuxiany, 10, 7)
+      yaxis = 40;
+      ellipse(i, yaxis, 10, 7)
     } else if (fre > 508 && fre < 554) {
-      wuxiany = 35;
-      ellipse(i, wuxiany, 10, 7)
+      yaxis = 35;
+      ellipse(i, yaxis, 10, 7)
     } else if (fre > 554 && fre < 622) {
-      wuxiany = 30;
-      ellipse(i, wuxiany, 10, 7)
+      yaxis = 30;
+      ellipse(i, yaxis, 10, 7)
     } else if (fre > 622 && fre < 680) {
-      wuxiany = 25;
-      ellipse(i, wuxiany, 10, 7)
+      yaxis = 25;
+      ellipse(i, yaxis, 10, 7)
     } else if (fre > 680 && fre < 740) {
-      wuxiany = 20;
-      ellipse(i, wuxiany, 10, 7)
+      yaxis = 20;
+      ellipse(i, yaxis, 10, 7)
     } else if (fre > 740 && fre < 830) {
-      wuxiany = 15;
-      ellipse(i, wuxiany, 10, 7)
+      yaxis = 15;
+      ellipse(i, yaxis, 10, 7)
     } else if (fre > 830) {
       fill(255, 0, 0);
       stroke(255, 0, 0);
       strokeWeight(1);
       text("TOO HIGH", 10, 20);
     }
-   
-  
 }
-
 
   fill(0, 0, 0); // black text
   text("centroid: ", 100, 200);
   text(round(fre) + " Hz", 100, 220);
 
-  for (var wuxianpu = 10; wuxianpu <= 60; wuxianpu = wuxianpu + 10) {
+  for (var staff = 10; staff <= 60; staff = staff + 10) {
     strokeWeight(1);
     stroke(0);
-    line(10, wuxianpu, 390, wuxianpu);
+    line(10, staff, 390, staff);
   }
 }
 
